2250bd39284d0210564eb6eb185559c0697f8ed9
yarina2917 <yarina297@gmail.com>
2019-12-05 22:15:52 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const { Transform } = require('stream');
+const fs = require('fs')
+const path = require('path')
+const { Transform } = require('stream')
 
-const dataFileName = path.join(__dirname, 'file.txt');
-const numbersFileName = path.join(__dirname, 'numbers.txt');
+const dataFileName = path.join(__dirname, 'file.txt')
+const numbersFileName = path.join(__dirname, 'numbers.txt')
 
 function createFile() {
     const writeStream = fs.createWriteStream(dataFileName)
     for (let i = 0; i < 10000; i++) {
         writeStream.write('Data for writing.\n')
     }
-    writeStream.end();
+    writeStream.end()
 }
 
 function addDataToFile() {
@@ -30,46 +30,46 @@ fs.stat(dataFileName, (err) => {
     }
     readFile()
     addDataToFile()
-});
+})
 
 const upperCaseTransform = new Transform({
     transform(chunk, encoding, callback) {
-        this.push(chunk.toString().toUpperCase());
-        callback();
+        this.push(chunk.toString().toUpperCase())
+        callback()
     }
-});
+})
 
 const deleteNumbersTransform = new Transform({
     transform(chunk, encoding, callback) {
-        const str = chunk.toString();
-        const numbers = str.match(/\d/g);
+        const str = chunk.toString()
+        const numbers = str.match(/\d/g)
         if (numbers) {
-            writeNumbersToFile(numbers.join(','));
+            writeNumbersToFile(numbers.join(','))
         }
-        this.push(str.replace(/\d/g, ''));
-        callback();
+        this.push(str.replace(/\d/g, ''))
+        callback()
     }
-});
+})
 
 function writeNumbersToFile(numbers) {
     const data = {
         numbers: numbers,
         date: new Date().toISOString(),
         name: 'Yana'
-    };
-    const writeStream = fs.createWriteStream(numbersFileName, {flags: 'a'});
-    writeStream.write(JSON.stringify(data) + '\n');
-    writeStream.end();
+    }
+    const writeStream = fs.createWriteStream(numbersFileName, {flags: 'a'})
+    writeStream.write(JSON.stringify(data) + '\n')
+    writeStream.end()
 }
 
 const textTransform = new Transform({
     transform(chunk, encoding, callback) {
-        const str = chunk.toString().toLowerCase();
-        this.push(str.charAt(0).toUpperCase() + str.slice(1));
-        callback();
+        const str = chunk.toString().toLowerCase()
+        this.push(str.charAt(0).toUpperCase() + str.slice(1))
+        callback()
     }
-});
+})
 
-process.stdin.pipe(upperCaseTransform).pipe(process.stdout);
-process.stdin.pipe(deleteNumbersTransform).pipe(process.stdout);
-process.stdin.pipe(textTransform).pipe(process.stdout);
+process.stdin.pipe(upperCaseTransform).pipe(process.stdout)
+process.stdin.pipe(deleteNumbersTransform).pipe(process.stdout)
+process.stdin.pipe(textTransform).pipe(process.stdout)
